71c016f6a8d278cae53ca67db5cf4fd965d96093
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-09-27 03:09:03 +0630

    Feat:user_seeder_create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,23 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = [
-            [
-                "user_name" => "Admin",
-                "email"     => "admin@gmail.com",
-                "password"  => "Ks82787294",
-                'role'      => "Admin"
-            ],
-            [
-                "user_name" => "User",
-                "email"     => "user@gmail.com",
-                "password"  => "Ks82787294",
-                'role'      => "Tenant"
-            ]
-        ];
-
-        foreach($users as $user) {
-            User::create($user);
-        }
+        $this->call([
+            UserSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                "user_name" => "Admin",
+                "email"     => "admin@gmail.com",
+                "password"  => "Ks82787294",
+                'role'      => "Admin"
+            ],
+            [
+                "user_name" => "User",
+                "email"     => "user@gmail.com",
+                "password"  => "Ks82787294",
+                'role'      => "Tenant"
+            ]
+        ];
+
+        foreach($users as $user) {
+            User::create($user);
+        }
+
+    }
+}
