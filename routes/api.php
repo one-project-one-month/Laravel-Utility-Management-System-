6f82f8df1c36903fbaa06ecad7008d1d428c52d4
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-09-27 00:27:39 +0630

    Feat:role_check_middleware_add

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware(['auth','Role.check:Admin'])->group(function() {
+
+    // Route::get('/products',function() {
+    //     return response()->json([
+    //         'message' => 'Hi'
+    //     ],200);
+    // });
+});
