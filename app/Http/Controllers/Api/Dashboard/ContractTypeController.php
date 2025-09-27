5959e77f33b6845a9543d65c28b1c252c243c1b1
AKThu <aungkhantthu002@gmail.com>
2025-09-28 01:17:26 +0700

    feat: created contract types index route

--- app/Http/Controllers/Api/Dashboard/ContractTypeController.php
+++ app/Http/Controllers/Api/Dashboard/ContractTypeController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Api\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Helpers\ApiResponse;
+use App\Http\Resources\Api\ContractTypeResource;
+use App\Models\ContractType;
 use Illuminate\Http\Request;
 
 class ContractTypeController extends Controller
 {
-    //
+    use ApiResponse;
+
+    /**
+     * Display a listing of contract types.
+     */
+    public function index()
+    {
+        $contractTypes = ContractType::paginate(10);
+
+        return $this->successResponse(
+            'Contract types retrieved successfully',
+            ContractTypeResource::collection($contractTypes),
+            200
+        );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,21 @@ use App\Http\Controllers\Api\Dashboard\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Dashboard\ContractTypeController;
 
 Route::post('/v1/auth/login',[AuthController::class,'login']);
 
 Route::prefix('v1')->group(function() {
 
 Route::middleware(['auth:sanctum',"Role.check:Admin"])->group(function() {
+    
     // Users
     Route::post('/users',[UserController::class,'create']);
     Route::get('/users', [UserController::class,'index']);
-
+    
+    // Contract Types
+    Route::get('/contract-types', [ContractTypeController::class, 'index']);
+    
 });
 
 Route::middleware(['auth:sanctum',"Role.check:Tenant"])->group(function() {
