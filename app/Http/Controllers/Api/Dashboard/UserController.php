35841510a6cb7699569e61a9a0d81b8686afb1ec
AungMinKo-tech <aungminko951357@gmail.com>
2025-09-27 19:54:39 +0630

    Feat:user_create_index_add

---
+++ app/Http/Controllers/Api/Dashboard/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api\Dashboard;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Helpers\ApiResponse;
+use Laravel\Sanctum\HasApiTokens;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\Api\UserResource;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class UserController extends Controller
+{
+    use ApiResponse, HasApiTokens, HasFactory, Notifiable;
+
+    //user create
+    public function store(Request $request)
+    {
+        //validation
+        $validator = Validator::make($request->all(),[
+            'userName' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|min:6|regex:/[0-9]/|regex:/[a-zA-Z]/',
+            'role' => 'required|in:Admin,Tenant,Staff',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->errors(), 422);
+        }
+
+        //create user
+        try {
+            $user = User::create([
+                'user_name' => $request->userName,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'role' => $request->role,
+                'tenant_id' => $request->tenant_id,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User created successfully',
+                'content' => new UserResource($user)
+            ], 201);
+
+        } catch (\Exception $e) {
+            return $this->errorResponse('User creation failed: ' . $e->getMessage(), 500);
+        }
+    }
+
+    //index users
+    public function index(){
+        $users = User::all();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Users retrieved successfully',
+            'content' => UserResource::collection($users)
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Dashboard\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
@@ -9,6 +10,9 @@ Route::post('/v1/auth/login',[AuthController::class,'login']);
 Route::prefix('v1')->group(function() {
 
 Route::middleware(['auth:sanctum',"Role.check:Admin"])->group(function() {
+    // Users
+    Route::post('/users',[UserController::class,'store']);
+    Route::get('/users', [UserController::class,'index']);
 
 });
 
