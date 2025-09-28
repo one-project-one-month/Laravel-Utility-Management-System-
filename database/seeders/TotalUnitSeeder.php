484fb42efd1d6e0704d0b2cc79dcacd4d729ebab
Kaung Sett Thu <168012673+KaungSettThu1873@users.noreply.github.com>
2025-09-29 05:33:11 +0630

    Feat:total_unit_seeder_add

---
+++ database/seeders/TotalUnitSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bill;
+use App\Models\TotalUnit;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TotalUnitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $bills = Bill::select('id','electricity_fee','water_fee')->get();
+
+        foreach ($bills as $bill) {
+            TotalUnit::Create([
+                    'bill_id'           => $bill->id,
+                    'electricity_units' => $bill->electricity_fee / config('units.electric'),
+                    'water_units'       => $bill->water_fee / config('units.water')
+            ]);
+        }
+    }
+}
