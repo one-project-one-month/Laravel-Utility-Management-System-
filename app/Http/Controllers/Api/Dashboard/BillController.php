37c3afb168769e12cd8845d3d0f5168debe864f5
MadThura <thura36094@gmail.com>
2025-09-30 22:20:02 +0630

    Feat:bill_index_and_show_add

--- app/Http/Controllers/Api/Dashboard/BillController.php
+++ app/Http/Controllers/Api/Dashboard/BillController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Api\Dashboard;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Helpers\ApiResponse;
+use App\Http\Resources\Api\Dashboard\BillResource;
+use App\Models\Bill;
 
 class BillController extends Controller
 {
-    //
+    use ApiResponse;
+
+    public function index()
+    {
+        $bills = Bill::with('totalUnit', 'invoice')->get();
+
+        if ($bills->isEmpty()) {
+            return $this->errorResponse('Not found!', 404);
+        }
+
+        return $this->successResponse('Bills retrieved successfully', BillResource::collection($bills));
+    }
+
+    public function show($id)
+    {
+        $bill = Bill::whereId($id)->with('totalUnit', 'invoice')->first();
+
+        if (! $bill) {
+            return $this->errorResponse('Not found!', 404);
+        }
+
+        return $this->successResponse('Bill retrieved successfully', new BillResource($bill),
+        );
+    }
 }
