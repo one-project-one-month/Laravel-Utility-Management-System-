4ff3362cddc247ef4dc5c3377faa5537fce4d0a9
Nyan Min Htet <austin770366@gmail.com>
2025-10-05 16:32:07 +0630

    Fix/api_route
    * fix:remove double UserController
    
    * fix:remove extra curly bracket
    
    ---------
    
    Co-authored-by: Kaung Sett Thu <168012673+KaungSettThu1873@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -79,5 +79,5 @@ Route::prefix('v1/')->group(function () {
         Route::get('/tenants/{id}/bills/latest', [ClientBillController::class,'latestBill']);
         Route::get('/tenants/{id}/bills/history', [ClientBillController::class,'billHistory']);
      });
-});
 
+    });
