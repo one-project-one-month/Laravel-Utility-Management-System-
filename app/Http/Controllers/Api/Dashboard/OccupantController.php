ea10bd98c84e2e1f65eb15eadccd12185b956074
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-10-13 07:30:08 +0630

    Feat/occupant_api_add

--- app/Http/Controllers/Api/Dashboard/OccupantController.php
+++ app/Http/Controllers/Api/Dashboard/OccupantController.php
@@ -2,18 +2,61 @@
 
 namespace App\Http\Controllers\Api\Dashboard;
 
+use Exception;
 use App\Models\Occupant;
 use Illuminate\Http\Request;
 use App\Http\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\Api\Dashboard\OccupantResource;
 
 class OccupantController extends Controller
 {
     use  ApiResponse ;
-    
+
     public function index() {
         $occupants = Occupant::get();
         return $this->successResponse('Occupants retrieved successfully',OccupantResource::collection($occupants));
+    }
+
+    public function update(Request $request,$id) {
+        try
+        {
+            $occupant = Occupant::findOrFail($id);
+
+            $validator = Validator::make($request->all(),[
+                'name' => 'required',
+                'nrc'  => 'nullable',
+                'relationshipToTenant' => 'required'
+            ]);
+
+            if($validator->fails()) {
+                return $this->errorResponse($validator->errors(),422);
+            }
+
+            $validatedData = $validator->validated();
+
+            $validated = [
+                'name' => $validatedData['name'],
+                'nrc'  => $validatedData['nrc'],
+                'relationship_to_tenant' => $validatedData['relationshipToTenant'],
+            ];
+
+            $occupant->update($validated);
+
+            return $this->successResponse('Occupant information updated successfully!',new OccupantResource($occupant));
+        } catch (\Exception $e) {
+            return $this->errorResponse($e->getMessage(),500);
+        }
+    }
+
+    public function show($id) {
+        $occupant = Occupant::find($id);
+
+        if(!$occupant) {
+            return $this->errorResponse('Occupant not found!',404);
+        }
+
+        return $this->successResponse('Occupant information found successfully!',new OccupantResource($occupant));
     }
 }
