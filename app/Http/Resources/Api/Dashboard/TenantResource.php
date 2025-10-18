fb7a017ae6ff4163eeb9f645e1bd3c78251cf6c6
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-10-18 17:53:44 +0630

    Fix:add_contract_obj_in_tenant

--- app/Http/Resources/Api/Dashboard/TenantResource.php
+++ app/Http/Resources/Api/Dashboard/TenantResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\Api\Dashboard;
 use Illuminate\Http\Request;
 use App\Http\Helpers\PostgresHelper;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Api\Dashboard\ContractResource;
 use App\Http\Resources\Api\Dashboard\OccupantResource;
 
 
@@ -73,7 +74,8 @@ class TenantResource extends JsonResource
                 'email'         => $this->email,
                 'phNumber'      => $this->phone_no,
                 'emergencyNo'   => $this->emergency_no,
-                'occupants'     => OccupantResource::collection($this->whenLoaded('occupants'))
+                'occupants'     => OccupantResource::collection($this->whenLoaded('occupants')),
+                'contracts'     => ContractResource::collection($this->whenLoaded('contracts'))
             ];
     }
 }
