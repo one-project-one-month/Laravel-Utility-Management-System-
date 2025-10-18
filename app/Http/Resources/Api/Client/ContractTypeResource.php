713dd4a856a2f347396afd7cf01a7797bc1bad1c
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2025-10-19 02:42:01 +0630

    refactor:client_swagger_docs

--- app/Http/Resources/Api/Client/ContractTypeResource.php
+++ app/Http/Resources/Api/Client/ContractTypeResource.php
@@ -5,6 +5,17 @@ namespace App\Http\Resources\Api\Client;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="ClientContractTypeResource",
+ *     title="Client Contract Resource",
+ *     description="Client-facing contract model representation",
+ *     @OA\Property(property="id", type="integer", example="1"),
+ *     @OA\Property(property="name", type="string", example="3 months"),
+ *     @OA\Property(property="duration", type="integer", example="3"),
+ *     @OA\Property(property="price", type="float", example="800000.00"),
+ * )
+ */
 class ContractTypeResource extends JsonResource
 {
     /**
--- app/Http/Resources/Api/Client/TenantResource.php
+++ app/Http/Resources/Api/Client/TenantResource.php
@@ -5,6 +5,38 @@ namespace App\Http\Resources\Api\Client;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="ClientTenantResource",
+ *     title="Client Tenant Resource",
+ *     description="Client-facing tenant model representation",
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         example="John Doe"
+ *     ),
+ *     @OA\Property(
+ *         property="nrc",
+ *         type="string",
+ *         example="12/ABC(N)123456"
+ *     ),
+ *     @OA\Property(
+ *         property="email",
+ *         type="string",
+ *         example="john.doe@example.com"
+ *     ),
+ *     @OA\Property(
+ *         property="phNumber",
+ *         type="string",
+ *         example="09123456789"
+ *     ),
+ *     @OA\Property(
+ *         property="emergencyNo",
+ *         type="string",
+ *         example="09111222333"
+ *     )
+ * )
+ */
 class TenantResource extends JsonResource
 {
     /**
