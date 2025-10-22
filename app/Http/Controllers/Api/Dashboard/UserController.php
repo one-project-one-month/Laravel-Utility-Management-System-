344d955187eaa3a85c737410a76841c5c6d3a74a
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-10-22 13:47:35 +0630

    Feat:tests for User API endpoints

--- app/Http/Controllers/Api/Dashboard/UserController.php
+++ app/Http/Controllers/Api/Dashboard/UserController.php
@@ -236,11 +236,12 @@ class UserController extends Controller
     public function show($id)
     {
         $user = User::find($id);
-        $user->load('tenant');
 
         if (!$user) {
             return $this->errorResponse('User not found.', 404);
         }
+
+        $user->load('tenant');
 
         return $this->successResponse('User Fetched Successfully', new UserResource($user));
     }
