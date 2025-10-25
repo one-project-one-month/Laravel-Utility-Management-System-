62230a25bb9919df717c43edcaa883e365a4d172
Nyan-MinHtet <austin770366@gmail.com>
2025-10-26 03:05:46 +0630

    feat:customer_service_test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * The base API URL for tests.
+     * @var string
+     */
+    public string $api; 
 }
