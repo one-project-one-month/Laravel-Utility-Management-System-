a48810d9833767eefcfe5d4b2eb6bb21dad47436
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-10-30 00:38:11 +0630

    Feat/bill_queue_service_format_change

--- app/Http/Services/Api/ReceiptService.php
+++ app/Http/Services/Api/ReceiptService.php
@@ -30,6 +30,10 @@ Class ReceiptService {
                 'other'       => $receipt->invoice->bill->fine_fee + $receipt->invoice->bill->service_fee + $receipt->invoice->bill->ground_fee + $receipt->invoice->bill->car_parking_fee,
                 'total'       => $receipt->invoice->bill->total_amount,
                 'due_date'    => $receipt->invoice->bill->due_date,
+                'invoice_no'  => $receipt->invoice->invoice_no,
+                'invoice_status' =>  $receipt->invoice->status,
+                'paid_date'    => $receipt->paid_date,
+                'payment_method'   =>  $receipt->payment_method
             ],
             $user->email,
             "Utility Receipt - " . \Carbon\Carbon::now()->format('F'),
